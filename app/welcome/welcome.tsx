f7c5dce81a662fcd5569bf7c94ed370a43193ed5
reece.mcgowan <reece.mcgowan@hotmail.com>
2026-03-15 10:32:26 -0700

    render some markdown in the center of the page

--- app/welcome/welcome.tsx
+++ app/welcome/welcome.tsx
@@ -1,5 +1,12 @@
+import Markdown from "react-markdown";
+
 export function Welcome() {
+
+  const markdown = '# Hi, *Pluto*!'
+
   return (
-    <div>hi</div>
+    <div className={"flex items-center"}>
+      <Markdown>{markdown}</Markdown>
+    </div>
   );
 }
