7effee887d6a2bada0bd30d03e34ae7e9a72ed8e
reece.mcgowan <reece.mcgowan@hotmail.com>
2026-03-16 21:10:24 -0700

    conditionally render error message

--- app/routes/blogPost.tsx
+++ app/routes/blogPost.tsx
@@ -8,26 +8,27 @@ export default function BlogPost() {
   let params = useParams();
   let blogPost = blogPosts.find(v => v.id == Number(params.id))
 
-  if (!blogPost) {
-    return <div>Failed to find blog post with ID {params.id}</div>
-  }
-
   return (
     <>
       <div className={"flex-col h-fit w-1/2"}>
         <Header />
-        <div>
-          <div className={"flex justify-start text-7xl font-iceland mt-5"}>
-            <div className={"w-fit"}>{blogPost.title}</div>
+        {blogPost ? (
+          <div>
+            <div className={"flex justify-start text-7xl font-iceland mt-5"}>
+              <div className={"w-fit"}>{blogPost.title}</div>
+            </div>
+            <div className={"flex justify-start text-xl"}>
+              <div className={"w-fit"}>Created {blogPost.uploadedDate}</div>
+            </div>
+            <hr className={"mt-4 mb-4"}/>
+            <div className={"markdown-content text-xl"}>
+              <Markdown rehypePlugins={[rehypeRaw]}>{blogPost.content}</Markdown>
+            </div>
           </div>
-          <div className={"flex justify-start text-xl"}>
-            <div className={"w-fit"}>Created {blogPost.uploadedDate}</div>
-          </div>
-          <hr className={"mt-4 mb-4"}/>
-          <div className={"markdown-content text-xl"}>
-            <Markdown rehypePlugins={[rehypeRaw]}>{blogPost.content}</Markdown>
-          </div>
-        </div>
+          ) : (
+            <div>Failed to find blog post with ID {params.id}</div>
+          // TODO Header doesn't work for some reason in this block?
+          )}
       </div>
     </>
   )
